425b1a6876f7cf3415a718eb58bcf399bccbb3ee
hiro <kaibonpoyopoyon@outlook.jp>
2022-06-02 09:02:25 +0900

    Settings function changed

--- hikariapp/settings.py
+++ hikariapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from .settings_local import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h31o&$t718p=zg^_em@x^at2f2-6_e9dzefs!bt32-6gv46f3f'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ hikariapp/settings_local_sample.py
@@ -0,0 +1,4 @@
+"Djangoの秘密キー"
+SECRET_KEY = ''
+
+"Databaseはsqlite3ならばそのままで良いです"
